257fe5acb0036dc545ea07d4aa157005de26032a
Glitch (shaded-hip) <none>
2019-03-18 11:09:50 +0000

    Setting up template engine

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const pug = require('pug');
 const app = express();
 
-app.set('view engine', 'pug');
+fccTesting(app); //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+//set up a template engine
+app.set('view engine','pug');
 
-app.get('/', (req, res) => {
-  res.render('views/pug/index.pug');
+app.route('/')
+.get((req, res) => {
+  res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Listening on port " + process.env.PORT);
 });
